c5cc447bbe4790c51ef97a0cd71d339365792ad2
Balaji <ampolu.balaji@gmail.com>
2014-10-21 19:58:06 +0530

    Project Added

---
+++ Angular_Save_ngroute/Controllers/PSDController.cs
@@ -0,0 +1,37 @@
+﻿using Angular_Save_ngroute.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Angular_Save_ngroute.Controllers
+{
+    public class PSDController : Controller
+    {
+        //
+        // GET: /PSD/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult Save(PSDViewModel PSD)
+        {
+            issEntities2 e = new issEntities2();
+            foreach (var obj in PSD.employees)
+            {
+                e.Employees.Add(obj);
+            }
+
+            if (e.SaveChanges() > 0)
+            {
+                return (Json(new { Status = "success" }, JsonRequestBehavior.AllowGet));
+            }
+
+            return (Json(new { Status = "failure" }, JsonRequestBehavior.AllowGet));
+        }
+
+    }
+}
---
+++ Angular_Save_ngroute/Models/PSDViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Angular_Save_ngroute.Models
+{
+    public class PSDViewModel
+    {
+        public List<Employee> employees { get; set; }
+
+        //public int EmpId { get; set; }
+        //public string Name { get; set; }
+        //public string DeptName { get; set; }
+    }
+}
